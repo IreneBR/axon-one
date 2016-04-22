a2a8337e119df4a6b037c721346a3b3c0c9d78d4
IreneBR <irenne.92@gmail.com>
2016-04-22 21:21:51 +0200

    Update axon.js

--- axon.js
+++ axon.js
@@ -3,7 +3,7 @@
 	//call the UI page "home"
 	App.load('Home');
 	var x = 5;
-	
+	var y = 0;
 	setInterval(function(){
 	
 		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/GPS", function( data ) {
