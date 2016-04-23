94b624bac2c58f3e61cb5a853a3f67ef6679db23
mistelip <mistelip@ethz.ch>
2016-04-23 02:05:12 +0200

    Update axon.js

--- axon.js
+++ axon.js
@@ -58,8 +58,8 @@ function storeNameValue(nam, val){
 	processData: false,
 	contentType: 'application/json',
 	//contentType: 'text',
-	data: '{"Name":"VALLL"}',
-	//data: '{nam: val}',
+	//data: '{"Name":"VALLL"}',
+	data: '{\"nam\": \"val\"}',
 	url: 'http://axonserver-mistelip.c9users.io:8080/',
 	success: function(json) { 
 		console.log('/sayHello POST was successful.'); 
